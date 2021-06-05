6d4f548af5b8d0f4782720ab38e695eaf317be2d
Atsushi Kawamura <kawamura@fringe81.com>
2021-06-05 20:18:59 +0900

    Initial Commit

---
+++ webpack.config.js
@@ -0,0 +1,33 @@
+var debug   = process.env.NODE_ENV !== "production";
+var webpack = require('webpack');
+var path    = require('path');
+
+module.exports = {
+  context: path.join(__dirname, "src"),
+  mode: "development",
+  entry: "./js/client.js",
+  devServer: {
+      contentBase: path.join(__dirname, "src"),
+      port:8080
+  },
+  module: {
+    rules: [{
+      test: /\.jsx?$/,
+        exclude: /(node_modules|bower_components)/,
+        use: [{
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-react', '@babel/preset-env']
+          }
+        }]
+      }]
+    },
+    output: {
+      path: __dirname + "/src/",
+      filename: "client.min.js"
+    },
+    plugins: debug ? [] : [
+      new webpack.optimize.OccurrenceOrderPlugin(),
+      new webpack.optimize.UglifyJsPlugin({ mangle: false, sourcemap: false }),
+    ]
+};
